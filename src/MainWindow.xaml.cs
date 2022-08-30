fbe62e817a7221dc3176635483371d4f852d5978
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-08-30 17:51:32 +0200

    update

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Office.Interop.Word;
+using Microsoft.Win32;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WordLinkValidator
 {
@@ -20,9 +10,89 @@ namespace WordLinkValidator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string[] filesToAnalyze;
+
         public MainWindow()
         {
+            filesToAnalyze = Array.Empty<string>();
+
             InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog {
+                Multiselect = true,
+                Filter      = "Word files (*.doc)|*.doc|Word files (*.docx)|*.docx"
+            };
+			
+            if (openFileDialog.ShowDialog() != true) {
+                return;
+            }
+
+            filesToAnalyze = openFileDialog.FileNames;
+        }
+
+        private void btnValidate_Click(object sender, RoutedEventArgs e)
+        {
+            if (filesToAnalyze == null || filesToAnalyze.Length < 1) {
+                MessageBox.Show("Please select Word files to analyze.", "Missing Documents", MessageBoxButton.OK);
+
+                return;
+            }
+
+            string text                                    = "";
+            Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+
+            foreach (string file in filesToAnalyze) {
+                Microsoft.Office.Interop.Word.Document doc = word.Documents.Open(file);
+
+                // Paragraphs
+                foreach (Microsoft.Office.Interop.Word.Paragraph para in doc.Paragraphs) {
+                    Microsoft.Office.Interop.Word.Range range = para.Range;
+                    text                                      = range.Text;
+
+                    if (hasLink(text)) {
+                        string[][] links = findLinks(text);
+
+                        foreach (string[] link in links) {
+                            // @TODO: validate link
+                            // @TODO: add validation result to list
+                        }
+                    }
+                }
+
+                // Footnotes
+                foreach (Microsoft.Office.Interop.Word.Footnote footnote in doc.Footnotes) {
+                }
+
+                // Endings
+                foreach (Microsoft.Office.Interop.Word.Endnote ending in doc.Endnotes) {
+                }
+
+                // doc.Hyperlinks
+               
+
+                // @TODO: check header and footer
+
+                doc.Close();
+            }
+
+            word.Quit();
+
+            filesToAnalyze = Array.Empty<string>();
+        }
+
+        private static bool hasLink(string text)
+        {
+            return text.Contains("http://")
+                || text.Contains("https://")
+                || text.Contains("file://");
+        }
+
+        private static string[][] findLinks(string text)
+        {
+            return new string[][] { Array.Empty<string>() };
         }
     }
 }
