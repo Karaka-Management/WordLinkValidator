17b228c2214ab27b8fab6a002a52ece3460ad5da
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-09-06 02:06:57 -0700

    update

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -8,7 +8,7 @@ namespace WordLinkValidator
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         private string[] filesToAnalyze;
 
